20f4184abe750af6e70122efa7def087a0646f1a
arya <you@example.com>
2024-04-03 10:03:35 +0200

    added header movies components with styling and updated the page

---
+++ src/components/header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header className="px-8 py-8">
+            <h1 className="text-center text-slate-950 font-bold text-lg">MyMovies</h1>
+        </header>
+    );
+}
---
+++ src/components/movies.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+export default function Movies() {
+    return (
+        <div>
+            <img src="https://placehold.co/150x200" className="max-w-40 rounded-md shadow-lg" width="150" height="200" />
+            <p className='py-2 font-bold'>SpiderMan</p>
+            <div className='flex gap-2'>
+                <FontAwesomeIcon className='text-amber-400 max-w-4' icon={faStar} />
+                <span className='text-black text-xs'>9/10 IMDb</span>
+            </div>
+        </div>
+    )
+}
