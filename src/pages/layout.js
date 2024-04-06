88e9186065303289dfb83915a10dacdc32ad8053
arya <you@example.com>
2024-04-06 22:29:00 +0200

    fixed layout and moved metadata to another directory

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,11 +1,13 @@
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
-
+import { metadata } from '../utilis/metadata';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <title>{metadata.title}</title>
+      <meta name="description" content={metadata.description} />
       <body className={inter.className}>{children}</body>
     </html>
   );
